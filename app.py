4301dcf281fac0fae8d648e2f7e896503ecb5f87
neopanis <88994765+neopanis@users.noreply.github.com>
2021-09-06 09:12:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello นางสาวปาณิสรา สินพันธุ์ เลขที่13 ม.4/8"
 
 @app.route('/api', methods=['GET'])
 def get_api():
